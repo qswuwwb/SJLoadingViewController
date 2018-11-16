09cff9929ac57165b3b8e1af25d04e1656ca8471
尹胜杰 <yinsj.init@gmail.com>
2018-11-16 14:52:13 +0800

    init

--- Example/SJLoadViewController/ViewController.swift
+++ Example/SJLoadViewController/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import SJLoadViewController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loadingView = LoadingView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        loadingView.backgroundColor = UIColor.black
+        view.addSubview(loadingView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
